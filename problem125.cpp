e93b7b8adadfb3476e95aa7ad03404c6a736e1f0
spawlacz <spawlaczyk97@gmail.com>
2020-11-26 13:33:09 +0100

    add 125 & 225

---
+++ problem125.cpp
@@ -0,0 +1,49 @@
+/**
+* Sebastian Pawlaczyk
+* solution for https://projecteuler.net/problem=125
+**/
+
+#include <cstdio>
+#include <cstdint>
+
+bool isPalindrome(uint32_t value)
+{
+    const uint32_t originalValue = value;
+    uint32_t reverseValue = value % 10;
+    value = value / 10;
+    while(value > 0)
+    {
+		reverseValue = (reverseValue * 10) + (value % 10);
+		value = value / 10;
+    }
+
+    return originalValue == reverseValue;
+}
+
+uint32_t findPalindromicSums(const uint32_t interval)
+{   
+    // first check for a^2 + b^2
+    uint32_t sum = 0;
+    for (uint32_t i = 1; i*i + (i+1)*(i+1) < interval; ++i)
+    {	
+    	uint32_t nextNumber = i + 1;
+		uint32_t squareSum = i*i + nextNumber*nextNumber;
+
+		while(squareSum < interval)
+		{
+		    if (isPalindrome(squareSum))
+		    {
+				sum = sum + squareSum;
+		    }
+		    ++nextNumber;
+		    squareSum = squareSum + nextNumber*nextNumber;
+		}
+    }
+    return sum;
+}
+
+int main()
+{
+	const uint32_t interval = 100000000;
+	printf("%u\n", findPalindromicSums(interval));
+}
---
+++ problem225.cpp
@@ -0,0 +1,46 @@
+/**
+* Sebastian Pawlaczyk
+* solution for https://projecteuler.net/problem=225
+**/
+
+#include <cstdio>
+#include <cstdint>
+
+uint32_t tribonacciNonDivisor(const uint32_t nth)
+{
+	uint32_t currentDivisor = 29;
+	uint32_t maxDivisor = 27;  //  optimization, starting from 1th knownw
+	uint32_t counter = 0;
+	while (counter != nth - 1)
+	{
+		int a = 1, b = 1, c = 1;
+
+		while (true)
+		{
+			uint32_t nextValue = (a + b + c) % currentDivisor;  // only modulo value neeeded
+			if (nextValue % currentDivisor == 0)
+			{
+				break;
+			}
+
+			a = b;
+			b = c;
+			c = nextValue;
+
+			if (a == 1 && b == 1 && c == 1)
+			{
+				maxDivisor = currentDivisor;
+				counter++;
+				break;
+			}
+		}
+		currentDivisor += 2;
+	}
+	return maxDivisor;
+}
+
+int main()
+{
+	const uint32_t nth = 124;
+	printf("%u\n", tribonacciNonDivisor(nth));
+}
